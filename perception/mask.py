5ec381958d437f30087917cb53e7bd39cf476f9d
William <williamxie00@gmail.com>
2024-09-10 15:42:31 -0600

    init

---
+++ perception/mask.py
@@ -0,0 +1,18 @@
+'''
+@file mask.py
+@brief segmentation wrapper to retrieve object mask(s), given image and input labels
+'''
+import os
+
+class Mask:
+    def __init__(self):
+        self.api_key = os.environ.get('SAM_API_KEY')
+        self.dims = None
+        self.H = None
+        self.W = None
+        self.boxes = None
+        self.labels = None
+        self.pred_dict = None
+
+    def __init__(self, ckpt=None, config=None, labels=None, threshold=0.5, device='cuda'):
+        pass
